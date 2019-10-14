8c48a72113f12022e8efddecfc98cfbe666ab47d
Denis Novikov <voothi@gmail.com>
2019-10-14 13:22:44 +0300

    i++ alt

--- src/ru/voothi/Main.java
+++ src/ru/voothi/Main.java
@@ -5,9 +5,9 @@ public class Main {
         int i, k;
         i = 0;
         k = 0;
-//        k = ++i;
-        i = i + 1;
+//        k = i++;
         k = i;
+        i = i + 1;
         System.out.println("k = " + k);
     }
 }
